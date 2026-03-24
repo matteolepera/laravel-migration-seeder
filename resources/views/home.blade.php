ac364cc5e808eb94ccfe05729fc4de069393246d
matteolpera <matteo.lepera02@gmail.com>
2026-03-24 15:39:17 +0100

    feat: stampato in pagina come test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,17 @@
 
 <body>
     <h1>Treni</h1>
+
+    <ul>
+        @foreach ($trains as $train)
+            <li>
+                {{ $train["company"] }}
+                {{ $train["departure_station"] }}
+                {{ $train["arrival_station"] }}
+                {{ $train["train_code"] }}
+            </li>
+        @endforeach
+    </ul>
 </body>
 
 </html>
